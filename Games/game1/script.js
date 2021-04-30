7657b1f1d7efbef8fb2702915c56689bf58c0b4d
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2021-04-30 09:09:50 +0530

    added game

--- Games/game1/script.js
+++ Games/game1/script.js
@@ -1,10 +1,10 @@
 class AudioController {
     constructor() {
-        this.bgMusic = new Audio('Assets/Audio/creepy.mp3');
-        this.flipSound = new Audio('Assets/Audio/flip.wav');
-        this.matchSound = new Audio('Assets/Audio/match.wav');
-        this.victorySound = new Audio('Assets/Audio/victory.wav');
-        this.gameOverSound = new Audio('Assets/Audio/gameOver.wav');
+        this.bgMusic = new Audio('./Assets/Audio/creepy.mp3');
+        this.flipSound = new Audio('./Assets/Audio/flip.wav');
+        this.matchSound = new Audio('./Assets/Audio/match.wav');
+        this.victorySound = new Audio('./Assets/Audio/victory.wav');
+        this.gameOverSound = new Audio('./Assets/Audio/gameOver.wav');
         this.bgMusic.volume = 0.5;
         this.bgMusic.loop = true;
     }
